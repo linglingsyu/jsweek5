4304bdbcb774c948218617dc9790afca217380ed
lingling <zzxcv741@hotmail.com>
2022-11-05 12:52:17 +0800

    get data use axios

--- src/app.js
+++ src/app.js
@@ -1,44 +1,20 @@
 const state = {
-  data: [
-    {
-      id: 0,
-      name: '肥宅心碎賞櫻3日',
-      imgUrl:
-        'https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80',
-      area: '高雄',
-      description: '賞櫻花最佳去處。肥宅不得不去的超讚景點！',
-      group: 87,
-      price: 1400,
-      rate: 10,
-    },
-    {
-      id: 1,
-      name: '貓空纜車雙程票',
-      imgUrl:
-        'https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
-      area: '台北',
-      description:
-        '乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感',
-      group: 99,
-      price: 240,
-      rate: 2,
-    },
-    {
-      id: 2,
-      name: '台中谷關溫泉會1日',
-      imgUrl:
-        'https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
-      area: '台中',
-      description:
-        '全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。',
-      group: 20,
-      price: 1765,
-      rate: 7,
-    },
-  ],
   flag: false,
-  init() {
+  data: [],
+  async init() {
+    await this.getData()
     this.setData()
+  },
+  async getData() {
+    try {
+      const response = await axios.get(
+        'https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json'
+      )
+      this.data = response.data.data
+      // console.log(this.data)
+    } catch (error) {
+      console.error(error)
+    }
   },
   setData(filterResult = false) {
     const contentBox = document.querySelector('#contentBox')
@@ -52,10 +28,12 @@ const state = {
       })
       filterCount.innerHTML = `本次共搜尋${filterResult.length}筆資料`
     } else {
+      console.log(this.data)
       this.data.forEach((item) => {
         const content = this.getDataTemplate(item)
         contentBox.insertAdjacentHTML('beforeend', content)
       })
+      filterCount.innerHTML = `本次共搜尋${this.data.length}筆資料`
     }
   },
   getDataTemplate(data) {
@@ -65,7 +43,7 @@ const state = {
     return `
     <li class="relative break-all basis-4/12 px-[15px]">
     <div class=" py-2 px-5 bg-[#64C3BF] text-white rounded-br rounded-tr absolute left-0 top-0 -translate-y-[15px] translate-x-[15px] z-10">${area}</div>
-    <div class="flex flex-col h-full overflow-hidden  rounded shadow-[0_3px_6px_1px_#00000029]">
+    <div class="flex flex-col h-full overflow-hidden bg-white rounded shadow-[0_3px_6px_1px_#00000029]">
       <a href="#">
       <img class="w-full h-[180px] object-cover ease-in duration-200 hover:scale-105 " src="${imgUrl}" alt="travel_${id}"></a>
       <div class="flex-grow flex flex-col relative p-5">
